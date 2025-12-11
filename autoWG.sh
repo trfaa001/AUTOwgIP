70574a0fa7fdbd842b19243faeca0ba065f68c5a
trfaa001 <trym@tovare.com>
2025-12-11 21:42:52 +0100

    Made a todo list

--- autoWG.sh
+++ autoWG.sh
@@ -9,7 +9,7 @@ echo "Host current public IP: " "$CURRENT_IP" " Host saved IP: " "$SAVED_IP"
 
 #Only changes the ip if the ip has changed
 if [ "$CURRENT_IP" != "$SAVED_IP" ]; then
-        ##printf "$CURRENT_IP" > /etc/wgAUTO/data.conf
+        printf "$CURRENT_IP" > /etc/wgAUTO/data.conf
 
         for CTID in $(pct list | awk 'NR>1 {print $1}'); do
                 echo "container" "$CTID" "found!"
@@ -18,6 +18,8 @@ if [ "$CURRENT_IP" != "$SAVED_IP" ]; then
                 echo "File $FILE_PATH exists in container $CTID"
 
                 pct exec "$CTID" -- sed -i "10s|.*|Endpoint = $CURRENT_IP:$PORT|" "$FILE_PATH"
+                pct exec "$CTID" -- wg-quick down wg0
+                pct exec "$CTID" -- wg-quick up wg0
             else
                 echo "No Wireguard comfig!"
             fi
