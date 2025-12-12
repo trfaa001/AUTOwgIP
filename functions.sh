b3a7467bc2556c96d8d396ace08d9a517d30f49a
trfaa001 <trym@tovare.com>
2025-12-12 13:23:24 +0100

    added logging, simple verification of port and ip,  and moved functions to a seperate functions bash

---
+++ functions.sh
@@ -0,0 +1,54 @@
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$0] $*" >>"$LOGFILE"
+}
+
+validate_port() {
+    local PORT="$1"
+    #Check if the port is in the rangr 1-65535
+    if ! [[ "$PORT" =~ ^[0-9]+$ ]] || [ "$PORT" -lt 1 ] || [ "$PORT" -gt 65535 ]; then
+        log "Error: PORT ($PORT) is not valid. Must be between 1 and 65535."
+        exit 1
+    fi
+}
+
+
+validate_ip() {
+    local IP="$1"
+
+    if [[ -z "$IP" ]]; then
+        log "Could not fetch current local IP!"
+        exit 3
+    fi
+
+    # Try IPv4 with ipcalc
+    if ipcalc -c "$IP" >/dev/null 2>&1; then
+        log "Valid ipcalc"
+        return 0
+    fi
+
+    # Try IPv6 with sipcalc or getent
+    if command -v sipcalc >/dev/null; then
+        if sipcalc "$IP" >/dev/null 2>&1; then
+            log "Valid sipcalc"
+            return 0
+        fi
+    else
+        if getent hosts "$IP" >/dev/null 2>&1; then
+            log "Valid getent"
+            return 0
+        fi
+    fi
+
+    log "Invalid IP"
+    exit 2
+    return 1
+}
+
+run_in_ct() {
+    local CTID="$1"; shift
+    
+    if ! pct exec "$CTID" -- "$@"; then
+        log "Error: command '$*' failed in container $CTID"
+        exit 4
+    fi
+}
---
+++ main.sh
@@ -0,0 +1,39 @@
+set -euo pipefail
+LOGFILE="/var/log/wgAUTO.log"
+
+source "functions.sh"
+
+#You may need to change these
+FILE_PATH="/etc/wireguard/wg0.conf" #File path to the wireguard config
+PORT="8473" #Wireguard port
+
+CURRENT_IP=$(curl -s ifconfig.me) #Can be replaced with other providers/services
+SAVED_IP=$(cat /etc/wgAUTO/data.conf)
+
+echo "Host current public IP: $CURRENT_IP Host saved IP: $SAVED_IP" 
+
+validate_port "$PORT"
+
+validate_ip "$CURRENT_IP"
+
+#Only changes the ip if the ip has changed
+if [ "$CURRENT_IP" != "$SAVED_IP" ]; then
+        printf "$CURRENT_IP" > /etc/wgAUTO/data.conf
+
+        for CTID in $(pct list | awk 'NR>1 {print $1}'); do
+                log "container" "$CTID" "found!"
+
+            if pct exec "$CTID" -- test -f "$FILE_PATH"; then
+                log "File $FILE_PATH exists in container $CTID"
+
+                run_in_ct "$CTID" cp "$FILE_PATH" "$FILE_PATH.bak"
+
+                log "[$CTID] Updating WireGuard endpoint to $CURRENT_IP:$PORT"
+                run_in_ct "$CTID" sed -i "10s|.*|Endpoint = $CURRENT_IP:$PORT|" "$FILE_PATH"
+                run_in_ct "$CTID" wg-quick down wg0
+                run_in_ct "$CTID" wg-quick up wg0
+            else
+                log "[$CTID] does not have the file $FILE_PATH"
+            fi
+        done
+fi
